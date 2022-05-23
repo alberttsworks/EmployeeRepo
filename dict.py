54bf12a92cd35e5678400ef83aa60b4461b2bf8f
alberttsworks <Albert.Ruby@tsworkz.com>
2022-05-23 15:09:00 +0530

    first commit

---
+++ dict.py
@@ -0,0 +1,7 @@
+crew_details={"Pilot":"Kumar","Co-Pilot":"Raghav","Head-Stewardess":"Malini","Stewardess":"Mala"}
+print(crew_details)
+print(crew_details["Pilot"])
+for key,value in crew_details.items():
+ print(key,":",value)
+
+
---
+++ dict1.py
@@ -0,0 +1,9 @@
+crew_details={"Pilot":"Kumar","Co-Pilot":"Raghav","Head-Stewardess":"Malini","Stewardess":"Mala"}
+print(crew_details)
+print("----printing keys frrom crew dictionary----")
+for key in crew_details.keys():
+ print(key)
+ 
+print("----printing values frrom crew dictionary----")
+for vals in crew_details.values():
+ print(vals)
